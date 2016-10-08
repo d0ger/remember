cf8bd8e433e76e1c7a27c363a7fd1e27e5fd2ca5
d0ger <d0ger@ya.ru>
2016-10-08 10:26:08 +0300

    add comment in set result page

--- www/application/views/test/set_result.php
+++ www/application/views/test/set_result.php
@@ -6,6 +6,8 @@
     <input type="hidden" name="result_id" value="<?=$item->result_id?>">
     <div><?=$item->question?></div>
     <br>
+    <div><?=$item->comment?></div>
+    <br>
     <div><?=$item->answer?></div>
     <br>
     <button type="submit" name="result" value="1">Верно</button>
